52d9a3986dc1f2d6f343b4c1235c631a3fb13534
kedvall <kanyon@life-foundry.com>
2025-03-12 08:55:23 +0800

    Restructure to simplify and streamline updates

--- pysync/__main__.py
+++ pysync/__main__.py
@@ -1,4 +1,6 @@
 import re
+from collections import defaultdict
+from dataclasses import dataclass, field
 from enum import Enum
 from pathlib import Path
 from typing import Annotated, Any, NamedTuple
@@ -6,38 +8,126 @@ from typing import Annotated, Any, NamedTuple
 import tomli
 import tomli_w
 import typer
-from rich.console import Console
-from packaging.specifiers import Specifier, SpecifierSet
+from packaging.specifiers import SpecifierSet
 from packaging.version import Version
+from rich.console import Console
 
 from pysync.uv import uv_sync
 
 app = typer.Typer()
 console = Console()
 
-# Regex that matches Python package names from https://packaging.python.org/en/latest/specifications/name-normalization/
-PACKAGE_NAME_REGEX = r"^([A-Z0-9][A-Z0-9._-]*[A-Z0-9]|[A-Z0-9])"
 
-
-class Package(NamedTuple):
-    name: str
-    version: Version
-    specifiers: SpecifierSet
-
-
-class VersionSpecifierOperators(str, Enum):
-    """
-    Package version specifiers. Note that some are intentionally excluded as they are not handled/would not be changed.
-    From https://packaging.python.org/en/latest/specifications/version-specifiers/#id5
-    """
+class VersionSpecifiers(str, Enum):
+    """Package version specifiers, see https://packaging.python.org/en/latest/specifications/version-specifiers/#id5"""
 
     COMPATIBLE = "~="
-    MATCHING = "=="
     GREATER_THAN = ">"
     GREATER_THAN_OR_EQUAL = ">="
 
 
-SUPPORTED_OPERATORS = [operator.value for operator in VersionSpecifierOperators]
+class Dependency(NamedTuple):
+    name: str
+    string: str
+    specifiers: SpecifierSet
+
+
+@dataclass
+class DependencyMap:
+    outdated: list[str] = field(default_factory=list)
+    dependency_names: set[str] = field(default_factory=set)
+    root_dependencies: list[Dependency] = field(default_factory=list)
+    additional_dependencies: defaultdict[str, list[Dependency]] = field(default_factory=lambda: defaultdict(list))
+
+    # Regex for Python package names, see https://packaging.python.org/en/latest/specifications/name-normalization/
+    _PACKAGE_NAME_REGEX = r"^([A-Z0-9][A-Z0-9._-]*[A-Z0-9]|[A-Z0-9])"
+
+    def add_dependency(self, raw_dependency: str, group: str | None = None) -> None:
+        dependency = self._get_dependency_info(raw_dependency)
+        self.dependency_names.add(dependency.name)
+        if group:
+            self.additional_dependencies[group].append(dependency)
+        else:
+            self.root_dependencies.append(dependency)
+
+    def _get_dependency_info(self, dependency: str) -> Dependency:
+        """Get detailed information about a specific dependency"""
+        match = re.search(self._PACKAGE_NAME_REGEX, dependency, re.IGNORECASE)
+        if not match:
+            console.print(f"[red]Failed to parse {dependency}, invalid package name")
+            raise typer.Exit(1)
+        version_specifiers = str(dependency[match.end() :])  # Trim package name
+        return Dependency(name=match[0], string=dependency, specifiers=SpecifierSet(version_specifiers))
+
+
+def get_dependencies(pyproject: dict[str, Any]) -> DependencyMap:
+    """Get a list of top-level project dependencies"""
+    dependency_map = DependencyMap()
+    # Add root-level dependencies
+    for dependency in pyproject["project"]["dependencies"]:
+        dependency_map.add_dependency(dependency)
+    # Add dependencies from dependency groups
+    for group, dependencies in pyproject.get("dependency-groups", {}).items():
+        for dependency in dependencies:
+            dependency_map.add_dependency(dependency, group)
+    return dependency_map
+
+
+def get_synced_dependency(dependency: Dependency, package_version: Version, updates: list[str]) -> str:
+    if not dependency.specifiers:
+        console.print(f"[red]- WARNING {dependency.name} has no version specifiers, pinning to >={package_version}")
+        updates.append(dependency.name)  # Add to the list of updated dependencies
+        return f"{dependency.name}>={package_version}"  # Missing specifiers, pin to >= current version
+
+    for specifier in dependency.specifiers:
+        if Version(specifier.version) == package_version:
+            continue  # Version is already up to date
+        if specifier.operator in [VersionSpecifiers.GREATER_THAN, VersionSpecifiers.GREATER_THAN_OR_EQUAL]:
+            console.print(f"- Bumping {dependency.name} from {specifier} to >={package_version}")
+            updates.append(dependency.name)  # Add to the list of updated dependencies
+            return dependency.string.replace(str(specifier), f">={package_version}")  # Replace returns a copy
+        elif specifier.operator is VersionSpecifiers.COMPATIBLE:
+            console.print(f"[red]- WARNING {specifier.operator} is not supported, skipping {dependency.specifiers}")
+
+    return dependency.string  # Nothing to update
+
+
+def sync_dependencies(workdir: Path) -> None:
+    """Sync pyproject.toml dependency versions with versions in the uv.lock file"""
+    with Path(workdir, "pyproject.toml").open("rb") as f:
+        pyproject = tomli.load(f)
+    with Path(workdir, "uv.lock").open("rb") as f:
+        packages: list[dict[str, Any]] = tomli.load(f)["package"]  # Get dict of packages from lockfile
+
+    # Get direct project dependencies and corresponding top-level packages from all dependency groups
+    dependency_map = get_dependencies(pyproject)
+    top_level_packages = {
+        package["name"]: Version(package["version"])
+        for package in packages
+        if package["name"] in dependency_map.dependency_names
+    }
+    console.print(
+        f"[bold cyan]\nSyncing dependency versions...[/]\n"
+        f"Found {len(top_level_packages)} top-level packages ({len(packages)} total)",
+        highlight=False,
+    )
+
+    # Bump dependency specifiers for root-level dependencies
+    updates: list[str] = []  # List of updated dependencies
+    pyproject["project"]["dependencies"] = [
+        get_synced_dependency(dep, top_level_packages[dep.name], updates) for dep in dependency_map.root_dependencies
+    ]
+    # Bump dependency specifiers for dependencies in dependency groups
+    pyproject["dependency-groups"] = {
+        group: [get_synced_dependency(dep, top_level_packages[dep.name], updates) for dep in dependencies]
+        for group, dependencies in dependency_map.additional_dependencies.items()
+    }
+
+    # Write updated pyproject.toml
+    with Path(workdir, "test_pyproject.toml").open("wb") as file:
+        tomli_w.dump(pyproject, file)
+
+    console.print(f"[bold green]Updated {len(updates)} dependencies")
 
 
 def workdir_callback(workdir_arg: Path) -> Path:
@@ -61,102 +151,8 @@ def sync(
     """Sync minimum dependency versions of the pyproject.toml and uv.lock files"""
     uv_sync(workdir, uv_passthrough_args)  # Call 'uv sync' as a subprocess to update lockfile
     sync_dependencies(workdir)
+    uv_sync(workdir, uv_passthrough_args, silent=True)
 
-
-def get_dependency_group_deps(pyproject: dict[str, Any]) -> list[str]:
-    """Get additional top-level dependencies from optional dependency groups (such as dev)"""
-    dependencies: list[str] = []
-    dependency_groups = pyproject.get("dependency-groups", {})
-    for group in dependency_groups.values():
-        dependencies.extend(group)
-    return dependencies
-
-
-def get_dependencies(pyproject: dict[str, Any]) -> dict[str, str]:
-    """Get a list of top-level project dependencies"""
-    project_deps = pyproject["project"]["dependencies"]
-    additional_deps = get_dependency_group_deps(pyproject)
-    top_level_deps: dict[str, str] = {}
-
-    for dep in project_deps + additional_deps:
-        match = re.search(PACKAGE_NAME_REGEX, dep, re.IGNORECASE)
-        if not match:
-            console.print(f"Failed to parse {dep}, invalid package name. Skipping...")
-            continue
-        version_specifiers = str(dep[match.end() :])  # Trim package name, keep as string to allow replacement
-        top_level_deps[match[0]] = version_specifiers  # match[0] is package name
-
-    return top_level_deps
-
-
-def needs_sync(package_version: Version, specifiers: SpecifierSet) -> bool:
-    return any(
-        spec for spec in specifiers if spec.operator in SUPPORTED_OPERATORS and Version(spec.version) != package_version
-    )
-
-
-def get_synced_version_specifier(package_version: Version, specifier: Specifier) -> Specifier:
-    match specifier.operator:
-        case VersionSpecifierOperators.COMPATIBLE:
-            return specifier  # TODO: Implement
-        case VersionSpecifierOperators.MATCHING:
-            return specifier  # TODO: Implement
-        case _:  # Greater than / greater than or equal
-            return Specifier(f">={package_version}")
-
-
-def get_synced_dependency(dependency: str, name: str, package_version: Version, specifiers: SpecifierSet) -> str:
-    for specifier in specifiers:
-        if specifier.operator in SUPPORTED_OPERATORS and Version(specifier.version) != package_version:
-            synced_version_specifier = get_synced_version_specifier(package_version, specifier)
-            console.print(f"- Bumped {name} from {specifier.version} to {synced_version_specifier.version}")
-            dependency = dependency.replace(str(specifier), str(synced_version_specifier))
-    return dependency
-
-
-def sync_dependencies(workdir: Path) -> None:
-    """Sync pyproject.toml dependency versions with versions in the uv.lock file"""
-    # Load pyproject.toml and uv.lock files
-    with Path(workdir, "pyproject.toml").open("rb") as f:
-        pyproject = tomli.load(f)
-    with Path(workdir, "uv.lock").open("rb") as f:
-        lockfile = tomli.load(f)
-
-    top_level_deps = get_dependencies(pyproject)  # Get direct project dependencies from all dependency groups
-    top_level_packages = [  # Get top-level packages
-        Package(name=pkg["name"], version=Version(pkg["version"]), specifiers=SpecifierSet(top_level_deps[pkg["name"]]))
-        for pkg in lockfile["package"]
-        if pkg["name"] in top_level_deps
-    ]
-    console.print(
-        "[bold]Syncing dependency versions...[/]\n"
-        f"Found {len(top_level_packages)} top-level packages ({len(lockfile['package'])} total)",
-        highlight=False
-    )
-
-    # Bump dependency specifier versions
-    updated_deps: list[str] = []
-    for package in top_level_packages:
-        if needs_sync(package.version, package.specifiers):
-            top_level_deps[package.name] = get_synced_dependency(
-                top_level_deps[package.name], package.name, package.version, package.specifiers
-            )
-            updated_deps.append(package.name)
-
-    # Create updated pyproject.toml file  # TODO: Improve this
-    for i, dep in enumerate(pyproject["project"]["dependencies"]):
-        name = re.search(PACKAGE_NAME_REGEX, dep, re.IGNORECASE)[0]  # match[0] is package name
-        pyproject["project"]["dependencies"][i] = f"{name}{top_level_deps[name]}"
-    for group, dependencies in pyproject.get("dependency-groups", {}).items():
-        for i, dep in enumerate(dependencies):
-            name = re.search(PACKAGE_NAME_REGEX, dep, re.IGNORECASE)[0]  # match[0] is package name
-            pyproject["dependency-groups"][group][i] = f"{name}{top_level_deps[name]}"
-
-    # Write updated pyproject.toml
-    with Path(workdir, "pyproject.toml").open("wb") as file:
-        tomli_w.dump(pyproject, file)
-
-    console.print(f"[bold green]Updated {len(updated_deps)} dependencies")
 
 if __name__ == "__main__":
     app()  # Support calling via python -m (as a module)
